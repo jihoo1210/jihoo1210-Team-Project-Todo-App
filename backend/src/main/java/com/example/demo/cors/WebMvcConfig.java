e93bc99781ffa2c8422872907255bcbcc57f659e
jihoo1210 <jihoostudy1@gmail.com>
2025-11-03 15:39:29 +0900

    cors 및 호환성 설정 (#3)
    Co-authored-by: ParkJihoo <jihoosyudy1@gmail.com>

--- backend/src/main/java/com/example/demo/cors/WebMvcConfig.java
+++ backend/src/main/java/com/example/demo/cors/WebMvcConfig.java
@@ -10,7 +10,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry
                 .addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "http://localhost:5173")
                 .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
